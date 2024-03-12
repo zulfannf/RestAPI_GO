c7e4a5b73433e394cb8f302bbb35806e45977c28
Z <122657299+zulfannf@users.noreply.github.com>
2024-03-12 18:03:42 +0700

    push

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import
+import (
+	"fmt"
+	"database/sql"
 
-func main (){
+	_ "github.com/lib/pq"
+)
 
+const (
+	host	= "localhost"
+	port	= 5432
+	user	= "postgres"
+	password = "akiyama23"
+	dbname = "postgres"
+
+)
+
+var (
+	db *sql.DB
+	err error
+)
+
+func main(){
+	psqlInfo := fmt.Sprintf("host=%s port=%d user=%s"+"password=%s dbname=%s sslmode=disable", host, port, user, password, dbname)
+
+	db,err = sql.Open("postgres", psqlInfo)
+	if err != nil {
+		panic(err)
+	}
+	defer db.Close()
+
+	err = db.Ping()
+	if err != nil{
+		panic(err)
+	}
+
+	fmt.Println("Successfully Connected to database")
 }

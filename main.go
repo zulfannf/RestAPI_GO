5f5cb726c22691e9a54aa1e45b4ae8bf9f97d9d5
Z <122657299+zulfannf@users.noreply.github.com>
2024-03-17 16:24:28 +0700

    init Update Orders and Items

--- main.go
+++ main.go
@@ -100,6 +100,26 @@ func Router(db *sql.DB){
 			})
 			return
 		}
+
+		orderIdStr := ctx.Param("orderId")
+    
+   		orderId, err := strconv.Atoi(orderIdStr)
+    	if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{
+            "success": false,
+            "error":   "invalid orderId",
+        })
+        return
+    }
+	err = UpdateOrderAndItems(db , ctx , req, orderId)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success" : false,
+				"error" : err.Error(),
+
+			})
+			return
+		}
 		ctx.JSON(http.StatusOK, map[string]interface{}{
 			"success" : true,
 			"order" : req,
@@ -279,4 +299,42 @@ func getOrders(db *sql.DB, ctx context.Context)([]Order,error){
 		orders = append(orders, order)
 	}
 	return orders, nil
+}
+
+func UpdateOrderAndItems(db *sql.DB, ctx context.Context, req UpdateOrderRequest, orderid int)(error){
+	tx, err:= db.Begin()
+		if err != nil {
+			return err
+		}
+
+		defer func ()  {
+			if err != nil {
+				tx.Rollback()
+			}
+		}()
+
+		res, err := tx.ExecContext(ctx, "update orders set customer_name = $1, ordered_at = $2 where order_id = $3", req.CustomerName, req.OrderedAt, orderid)
+		if err != nil {
+			return err
+		}
+		if rowsAffected, err := res.RowsAffected(); err != nil {
+			return err
+			} else if rowsAffected < 1{
+			return fmt.Errorf("Not Found")
+		}
+		
+		for _, item := range req.Items{
+			res, err := tx.ExecContext(ctx, 
+				"update items set item_code = $1, description = $2, quantity = $3 where item_id = $4 and order_id = $5",
+			item.ItemCode, item.Description, item.Quantity, item.LineItemId, orderid)
+			if err != nil {
+				return err
+			}
+			if rowsAffected, err := res.RowsAffected(); err != nil {
+				return err
+				} else if rowsAffected < 1{
+				return fmt.Errorf("Not Found")
+			}
+		} 
+			return tx.Commit()
 }

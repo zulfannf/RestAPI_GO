47856400f8546b44d1c2d45fd948ad4a9f8f2f44
Z <122657299+zulfannf@users.noreply.github.com>
2024-03-17 15:44:39 +0700

    update delete

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"net/http"
+	"strconv"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -95,7 +96,27 @@ func Router(db *sql.DB){
 	})
 
 	router.DELETE("/orders/:orderId", func(ctx *gin.Context) {
-		ctx.JSON(http.StatusCreated, map[string]interface{}{
+		orderIdStr := ctx.Param("orderId")
+    
+   		orderId, err := strconv.Atoi(orderIdStr)
+    	if err != nil {
+        ctx.JSON(http.StatusBadRequest, gin.H{
+            "success": false,
+            "error":   "invalid orderId",
+        })
+        return
+    }
+	err = deleteOrder(db , ctx , orderId)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success" : false,
+				"error" : err.Error(),
+
+			})
+			return
+		}
+
+		ctx.JSON(http.StatusOK, map[string]interface{}{
 			"success" : true,
 		})
 	})
@@ -197,4 +218,35 @@ func createOrderAndItems(db *sql.DB, ctx context.Context,req CreateOrderRequest)
 			}
 
 			return int(orderID),nil
+}
+
+func deleteOrder(db *sql.DB, ctx context.Context,orderID int)(error){
+	tx, err:= db.Begin()
+		if err != nil {
+			return err
+		}
+
+		defer func ()  {
+			if err != nil {
+				tx.Rollback()
+			}
+		}()
+
+		_,err = tx.ExecContext(ctx, "Delete from items where order_id = $1", orderID)
+		if err != nil {
+			return err
+		}
+
+		res,err := tx.ExecContext(ctx, "Delete from orders where order_id = $1", orderID)
+		if err != nil {
+			return err
+		}
+
+		if rowsAffected, err := res.RowsAffected(); err != nil {
+			return err
+			} else if rowsAffected < 1{
+			return fmt.Errorf("Not Found")
+		}
+		
+		return tx.Commit()
 }

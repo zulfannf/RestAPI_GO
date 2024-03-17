51403f5b1c020653c517e449c8fa8fa59d2ff814
Z <122657299+zulfannf@users.noreply.github.com>
2024-03-17 15:28:05 +0700

    add function insert order and items

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package main
 
 import (
+	"context"
 	"database/sql"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/gin-gonic/gin"
 	_ "github.com/lib/pq"
@@ -40,10 +42,10 @@ func main(){
 
 	fmt.Println("Successfully Connected to database")
 
-	Router()
+	Router(db)
 }
 
-func Router(){
+func Router(db *sql.DB){
 	router := gin.Default()
 	router.POST("/orders", func(ctx *gin.Context) {
 		req := CreateOrderRequest{}
@@ -54,9 +56,20 @@ func Router(){
 			})
 			return
 		}
+
+		orderId, err := createOrderAndItems(db , ctx , req)
+		if err != nil {
+			ctx.AbortWithStatusJSON(http.StatusInternalServerError, gin.H{
+				"success" : false,
+				"error" : err.Error(),
+
+			})
+			return
+		}
+	
 		ctx.JSON(http.StatusCreated, map[string]interface{}{
 			"success" : true,
-			"order" : req,
+			"order_id" : orderId,
 		})
 	})
 
@@ -117,4 +130,71 @@ type UpdateItemOrderRequest struct {
 	ItemCode string `json:"itemCode"`
 	Description string `json:"description"`
 	Quantity int `json:"quantity"`
+}
+
+type Item struct{
+	ItemId int 
+	ItemCode string
+	Description string
+	Quantity int
+	OrderId int
+}
+
+type Order struct {
+	OrderId int
+	CustomerName string
+	OrderedAt time.Time
+}
+
+func createOrderAndItems(db *sql.DB, ctx context.Context,req CreateOrderRequest)(int,error){
+	orderedAt, _ := time.Parse(time.RFC3339,req.OrderedAt)
+		
+	
+		order := Order{
+			CustomerName: req.CustomerName,
+			OrderedAt: orderedAt,
+
+		}
+
+		tx, err:= db.Begin()
+		if err != nil {
+			return 0,err
+		}
+
+		defer func ()  {
+			if err != nil {
+				tx.Rollback()
+			}
+		}()
+
+		var orderID int64
+		err = tx.QueryRowContext(ctx, "insert into orders(customer_name, ordered_at) values($1, $2) returning order_id",order.CustomerName, order.OrderedAt).Scan(&orderID)
+		if err != nil {
+			return 0,err
+		}
+		
+		
+			var items[]Item
+			for _, item := range req.Items{
+				items = append(items, Item{
+					ItemCode: item.ItemCode,
+					Description: item.Description,
+					Quantity: item.Quantity,
+					OrderId: int(orderID),
+				})
+			}
+
+			for _, item := range items {
+				_, err := tx.ExecContext(ctx, "INSERT INTO items (item_code, description, quantity, order_id) VALUES ($1, $2, $3, $4)", item.ItemCode, item.Description, item.Quantity, item.OrderId)
+				if err != nil {
+					return 0,err
+				}
+			}
+
+			err = tx.Commit()
+			if err != nil {
+				return 0,err
+			}
+
+			return int(orderID),nil
 }
